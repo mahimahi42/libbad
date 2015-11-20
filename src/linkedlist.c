7c0f37e2d2aa0af71e8db83032d7af088b2ddd64
Bryce Davis <me@bryceadavis.com>
2015-11-19 19:01:37 -0500

    Add linked list print function

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -6,11 +6,28 @@
  * @copyright Copyright (c) 2015, Bryce Davis. Released under the MIT License.
  *            See LICENSE.txt for details.
  */
-#include "linkedlist.h"
+#include <stdio.h>
 
-void llist_init(lb_llist* list)
+#include "linkedlist.h"
+#include "util.h"
+
+void llist_init(lb_llist list)
 {
 	list->head = NULL;
 	list->tail = NULL;
 	list->length = 0;
+}
+
+void llist_print(lb_llist list)
+{
+	if (list->head == NULL) {
+		printf("Empty list\n");
+	} else {
+		llist_node n = list->head;
+		while (n != NULL) {
+			data_print_str(n->data);
+			puts("");
+			n = n->next;
+		}
+	}
 }
